bbea1ece07e5127e9bd513b625781e36a7dfcd65
s.mule <s.mule@sangiorgisrl.it>
2023-12-12 09:58:13 +0100

    Add snapshot test for menu component;

---
+++ src/components/__test__/__snapshot__/menuComponent.test.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {render} from '@testing-library/react-native';
+import {useTheme} from 'react-native-paper';
+import {useNavigation} from '@react-navigation/native';
+import MenuButton from '../../menu/MenuComponent.tsx';
+
+// Mock the useNavigation and useTheme hooks
+jest.mock('@react-navigation/native');
+jest.mock('react-native-paper', () => ({
+  ...jest.requireActual('react-native-paper'),
+  useTheme: jest.fn(),
+}));
+
+describe('MenuButton snapshot', () => {
+  it('renders correctly', () => {
+    // Mock the navigate function otherwise error appear
+    const mockNavigate = jest.fn();
+
+    // Mock the useNavigation hook otherwise error appear
+    (useNavigation as jest.Mock).mockImplementation(() => ({
+      navigate: mockNavigate,
+    }));
+
+    // Mock the useTheme hook otherwise error appear
+    (useTheme as jest.Mock).mockReturnValue({
+      colors: {
+        primary: 'mockPrimaryColor',
+      },
+    });
+
+    const {toJSON} = render(<MenuButton />);
+    expect(toJSON()).toMatchSnapshot();
+  });
+});
